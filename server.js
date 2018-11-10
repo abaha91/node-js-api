e58cce5f588fe1ec73a031fd9352e38721873c5e
Gennady <abashkin.91@mail.ru>
2018-11-10 13:59:26 +0300

    Realized functions for different requests(POST, PUT, DELETE)

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
 let artists = [
     {
@@ -25,11 +31,35 @@ app.get('/artists', (req, res) => {
 });
 
 app.get('/artists/:id', (req, res) => {
-    console.log(req.params);
     let artist = artists.find((artist) => {
         return artist.id === Number(req.params.id);
-    })
+    });
+
     res.send(artist);
+});
+
+app.post('/artists', (req, res) => {
+    let artist = {
+        id: Date.now(),
+        name: req.body.name,
+    };
+
+    artists.push(artist);
+    res.send(artist);
+});
+
+app.put('/artists/:id', (req, res) => {
+    let artist = artists.find((artist) => {
+        return artist.id === Number(req.params.id);
+    });
+    artist.name = req.body.name;
+    res.send(artist);
+});
+
+app.delete('/artists/:id', (req, res) => {
+    artists = artists.filter(artist => {
+        return artist.id !== Number(req.params.id);
+    });
 });
 
 app.listen(3012, () => {console.log('API app started')});
