299af90e6b1450bed1d3f33f9831f21683143d0d
Gennady <abashkin.91@mail.ru>
2018-11-10 12:17:58 +0300

    Realized simple routes

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello API')
+let artists = [
+    {
+        id: 1,
+        name: 'Metallica',
+    },
+    {
+        id: 2,
+        name: 'Iron Maiden',
+    },
+    {
+        id: 3,
+        name: 'Deep Purple',
+    },
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello API');
+});
+
+app.get('/artists', (req, res) => {
+    res.send(artists);
+});
+
+app.get('/artists/:id', (req, res) => {
+    console.log(req.params);
+    let artist = artists.find((artist) => {
+        return artist.id === Number(req.params.id);
+    })
+    res.send(artist);
 });
 
 app.listen(3012, () => {console.log('API app started')});
